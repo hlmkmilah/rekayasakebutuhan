c9111eb785579386a8e833415374302c9365d6f6
theopratama@live.com <theopratama@live.com>
2017-05-08 22:46:42 +0700

    Edit style + login page

--- header.php
+++ header.php
@@ -35,3 +35,19 @@
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
 </head>
+<body class="hold-transition skin-blue sidebar-mini">
+    <header class="main-header">
+    <!-- Logo -->
+    <a href="index2.html" class="logo" style="background-color: white; color:black">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>A</b>LT</span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b>POS</b>Dashboard</span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top" style="background-color: white;">
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav"></ul>
+    </div>
+    </nav>
+  </header>
--- loginPage.php
+++ loginPage.php
@@ -1,7 +1,20 @@
 <!DOCTYPE html>
 <html>
     <?php require_once './header.php'; ?>
-    <?php require_once './sidebar.php'; ?>
-    <?php require_once './mainpage.php';?>
+    <div class="row">
+            <br>
+            <div class="col-md-4 col-md-offset-4" style="border:1px solid gray;padding:20px;">
+                <label for="exampleInputEmail1">Username</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" >
+            
+            
+                <label for="exampleInputEmail1">Password</label>
+                <input type="password" class="form-control" id="exampleInputEmail1" >
+            
+                <a href="">Lupa Password</a>
+                
+                <input type="submit" class="form-control" style="margin-top:20px;">
+            </div>
+    </div>
     <?php require_once './footer.php' ;?>
 </html>
