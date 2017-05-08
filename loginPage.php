552e92180e4a28fd0718879394ddace5f5478cf2
theopratama@live.com <theopratama@live.com>
2017-05-08 21:08:59 +0700

    Init project, refaktor kode

---
+++ loginPage.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+    <?php require_once './header.php'; ?>
+    <?php require_once './sidebar.php'; ?>
+    <?php require_once './mainpage.php';?>
+    <?php require_once './footer.php' ;?>
+</html>
