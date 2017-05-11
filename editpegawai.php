42f6640bd631316de41509f81dad7bd015e81676
Sabila Malahati A <bellaarnindya@gmail.com>
2017-05-11 12:04:50 +0700

    edit form password, telepon, status

--- editpegawai.php
+++ editpegawai.php
@@ -2,9 +2,6 @@
 <html>
 <?php require_once './header.php'; ?>
 <?php require_once './sidebar.php'; ?>
-
-
-
 <div class="box box-info">
     <div class="box-header with-border">
         <h3 class="box-title">Edit Pegawai</h3>
@@ -54,14 +51,14 @@
                 </div>
             </div>
             <div class="form-group">
-                <label for="inputEmail3" class="col-sm-2 control-label">Password</label>
+                <label for="inputEmail3" class="col-sm-2 control-label">Kata Sandi</label>
 
                 <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputEmail3" placeholder="Password">
+                    <input type="password" class="form-control" id="inputEmail3" placeholder="Masukkan kata sandi yang baru">
                 </div>
             </div>
             <div class="form-group">
-                <label for="inputEmail3" class="col-sm-2 control-label">No. HP</label>
+                <label for="inputEmail3" class="col-sm-2 control-label">Telepon</label>
 
                 <div class="col-sm-10">
                     <input type="text" class="form-control" id="inputEmail3" placeholder="081254359355">
@@ -78,21 +75,21 @@
                 <label for="inputEmail3" class="col-sm-2 control-label">Status</label>
 
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="inputEmail3" placeholder="Status">
+                    <form>
+                        <input type="radio" name="status" value="aktif" checked> Aktif<br>
+                        <input type="radio" name="status" value="tidak_aktif"> Tidak Aktif<br>
+                    </form>
                 </div>
             </div>
         </div>
         <!-- /.box-body -->
         <div class="box-footer">
-            <button type="submit" class="btn btn-default">Cancel</button>
+            <button type="submit" class="btn btn-default">Batal</button>
             <button type="submit" class="btn btn-info pull-right">Simpan</button>
         </div>
         <!-- /.box-footer -->
     </form>
 </div>
-
-
-
 <?php require_once './footer.php' ;?>
 </html>
 `
