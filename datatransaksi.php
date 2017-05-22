c46c4c54f05c329e9291f10b28307dae498392b4
Sabila Malahati A <bellaarnindya@gmail.com>
2017-05-22 20:51:18 +0700

    halaman transaksi

---
+++ datatransaksi.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<script src="plugins/jQuery/jquery-2.2.3.min.js" type="text/javascript"></script>
+<?php require_once './header.php'; ?>
+<?php require_once './sidebar.php'; ?>
+<section class="content">
+    <div class="box box-info">
+        <div class="box-header with-border">
+            <h3 class="box-title">Rincian Transaksi</h3>
+        </div>
+        <div class="box-body">
+            <table class="table table-bordered">
+                <thead align="center">
+                <tr>
+                    <td><b>ID Transaksi</b></td>
+                    <td><b>Tanggal</b></td>
+                    <td><b>Waktu</b></td>
+                    <td><b>Kasir</b></td>
+                    <td><b>Nama Barang</b></td>
+                    <td><b>Harga</b></td>
+                </tr>
+                </thead>
+                <tr>
+                    <td>POS001</td>
+                    <td>01/05/2017</td>
+                    <td>11.23</td>
+                    <td>Sabila Malahati Arnindya</td>
+                    <td>Oceana</td>
+                    <td>6000</td>
+                </tr>
+                <tr>
+                    <td>POS002</td>
+                    <td>01/05/2017</td>
+                    <td>11.34</td>
+                    <td>Nafia Rizky Yogayana</td>
+                    <td>Club 600 ml</td>
+                    <td>2500</td>
+                </tr>
+                <tr>
+                    <td>POS003</td>
+                    <td>01/05/2017</td>
+                    <td>11.35</td>
+                    <td>Nafia Rizky Yogayana</td>
+                    <td>Indomie Goreng</td>
+                    <td>3000</td>
+                </tr>
+                <tr>
+                    <td>POS004</td>
+                    <td>01/05/2017</td>
+                    <td>11.50</td>
+                    <td>Sabila Malahati Arnindya</td>
+                    <td>Tango</td>
+                    <td>8500</td>
+                </tr>
+                <tr>
+                    <td>POS005</td>
+                    <td>01/05/2017</td>
+                    <td>15.15</td>
+                    <td>Sabila Malahati Arnindya</td>
+                    <td>Marie Regal</td>
+                    <td>1000</td>
+                </tr>
+                <tr>
+                    <td>POS006</td>
+                    <td>01/05/2017</td>
+                    <td>18.00</td>
+                    <td>Nafia Rizky Yogayana</td>
+                    <td>Pop Mie Goreng</td>
+                    <td>5000</td>
+                </tr>
+                <tr>
+                    <td>POS007</td>
+                    <td>02/05/2017</td>
+                    <td>14.45</td>
+                    <td>Nafia Rizky Yogayana</td>
+                    <td>Rinso</td>
+                    <td>11000</td>
+                </tr>
+                <tr>
+                    <td>POS008</td>
+                    <td>02/05/2017</td>
+                    <td>15.31</td>
+                    <td>Farhan Maulana</td>
+                    <td>Aqua 250ml</td>
+                    <td>500</td>
+                </tr>
+                <tr>
+                    <td>POS009</td>
+                    <td>02/05/2017</td>
+                    <td>17.13</td>
+                    <td>Sabila Malahati Arnindya</td>
+                    <td>Pocari Sweat</td>
+                    <td>9000</td>
+                </tr>
+                <tr>
+                    <td>POS010</td>
+                    <td>03/05/2017</td>
+                    <td>10.10</td>
+                    <td>Farhan Maulana</td>
+                    <td>Chitato</td>
+                    <td>6500</td>
+                </tr>
+                <tr>
+                    <td>POS011</td>
+                    <td>02/05/2017</td>
+                    <td>11.42</td>
+                    <td>Sabila Malahati Arnindya</td>
+                    <td>Club 1500ml</td>
+                    <td>6000</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</section>
+
+
+<?php require_once './footer.php' ;?>
+</html>
