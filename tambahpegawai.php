daeff63203139c4f8acc0e27136bbabbf5263009
Sabila Malahati A <bellaarnindya@gmail.com>
2017-05-22 15:41:43 +0700

    tambah field password

--- tambahpegawai.php
+++ tambahpegawai.php
@@ -2,7 +2,8 @@
 <html>
 <?php require_once './header.php'; ?>
 <?php require_once './sidebar.php'; ?>
-<div class="content-wrapper">
+
+<section class="content">
     <div class="box box-info">
         <div class="box-header with-border">
             <h3 class="box-title">Tambah Pegawai</h3>
@@ -11,22 +12,16 @@
         <!-- form start -->
         <form class="form-horizontal">
             <div class="box-body">
-
-                <div class="form-group">
-                    <img class="profile-user-img img-responsive img-circle control-label" src="dist/img/user2-160x160.jpg" alt="User profile picture">
-                    <br>
-                    <input type="file" style="margin: 0 auto" id="exampleInputFile">
-                </div>
-
                 <div class="form-group">
                     <label for="inputEmail3" class="col-sm-2 control-label">ID</label>
 
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputEmail3" placeholder="PG0001" disabled>
+                        <input type="text" class="form-control" id="inputEmail3" placeholder="ID">
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="inputEmail3" class="col-sm-2 control-label">Nama</label>
+
 
                     <div class="col-sm-10">
                         <input type="text" class="form-control" id="inputEmail13" placeholder="Nama">
@@ -34,21 +29,23 @@
                 </div>
                 <div class="form-group">
                     <label for="inputEmail3" class="col-sm-2 control-label">Bagian</label>
-
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputEmail3" placeholder="Bagian">
+                        <select class="form-control" id="inputEmail3" name="Bagian">
+                            <option value="admin">Admin</option>
+                            <option value="akuntan">Akuntan</option>
+                            <option value="kasir">Kasir</option>
+                            <option value="gudang">Gudang</option>
+                        </select>
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-
                     <div class="col-sm-10">
                         <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="inputEmail3" class="col-sm-2 control-label">Password</label>
-
                     <div class="col-sm-10">
                         <input type="password" class="form-control" id="inputEmail3" placeholder="Password">
                     </div>
@@ -62,16 +59,19 @@
                 </div>
                 <div class="form-group">
                     <label for="inputEmail3" class="col-sm-2 control-label">Gaji</label>
-
                     <div class="col-sm-10">
                         <input type="text" class="form-control" id="inputEmail3" placeholder="Gaji">
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="inputEmail3" class="col-sm-2 control-label">Status</label>
-
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputEmail3" placeholder="Status">
+                        <div class="radio">
+                            <label><input type="radio" name="status" value="aktif">Aktif</label>
+                        </div>
+                        <div class="radio">
+                            <label><input type="radio" name="status" value="nonaktif">Non Aktif</label>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -83,6 +83,6 @@
             <!-- /.box-footer -->
         </form>
     </div>
-</div>
+</section>
 <?php require_once './footer.php' ;?>
 </html>
