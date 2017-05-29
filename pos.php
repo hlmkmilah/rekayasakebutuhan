f19bd5719274a823c78b23c52398596cc54e63dd
Sabila Malahati A <bellaarnindya@gmail.com>
2017-05-29 10:01:10 +0700

    update pos

--- pos.php
+++ pos.php
@@ -13,93 +13,121 @@
 <div class="row">
     <div class="col-xs-10 col-xs-offset-1">
         <div class="row">
-            <div class="col-xs-6">
-                <div class="box">
-                    <div class="box-header">
-                        <p>TOTAL BIAYA</p>
+            <div class="row">
+                <div class="col-xs-6">
+                    <div class="box">
+                        <div class="box-header">
+                            <p>Rincian</p>
+                        </div>
+                        <div class="box-body">
+                            <table class="table table-bordered">
+                                <thead align="center">
+                                <tr>
+                                    <td><b>ID Barang</b></td>
+                                    <td><b>Nama Barang</b></td>
+                                    <td><b>Jumlah</b></td>
+                                    <td><b>Harga</b></td>
+                                </tr>
+                                </thead>
+                                <tr>
+                                    <td>BDSD12321</td>
+                                    <td>Oceana</td>
+                                    <td>1</td>
+                                    <td>6000</td>
+                                </tr>
+                                <tr>
+                                    <td>BDSD12321</td>
+                                    <td>Rinso Cair 250ml</td>
+                                    <td>1</td>
+                                    <td>10000</td>
+                                </tr>
+                                <tr>
+                                    <td>BDSD12231</td>
+                                    <td>Baygon Cair 250ml</td>
+                                    <td>1</td>
+                                    <td>12000</td>
+                                </tr>
+                                <tr>
+                                    <td>BDSD12444</td>
+                                    <td>Indomie Rebus Ayam Bawang</td>
+                                    <td>1</td>
+                                    <td>3000</td>
+                                </tr>
+                            </table>
+                        </div>
                     </div>
-                    <div class="box-body">
-                        <h2><b>Rp.</b>31.000</h2>
-                        <p>Perintah :</p>
-                        <input type="text" value="1 BDSD12322">
-                        <input type="submit" value="OK">
-                        <p>Tekan <b>OK</b> untuk menjalankan perintah</p>
+                </div>
+                <div class="col-xs-6">
+                    <div class="box">
+                        <div class="box-header">
+                        </div>
+                        <div class="box-body">
+                            <div class="col-xs-6">
+                                <p>Kode barang:</p>
+                                <input type="text" value="BDSD12322">
+                            </div>
+                            <div class="col-xs-6">
+                                <h1><b>Rp 31.000</b></h1>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
+            <div class="col-xs-5 pull-right">
+                <div class="row">
+                    <button style="width:100px;height:100px;">7</button>
+                    <button style="width:100px;height:100px;">8</button>
+                    <button style="width:100px;height:100px;">9</button>
+                </div>
+                <div class="row">
+                    <button style="width:100px;height:100px;">4</button>
+                    <button style="width:100px;height:100px;">5</button>
+                    <button style="width:100px;height:100px;">6</button>
+                </div>
+                <div class="row">
+                    <button style="width:100px;height:100px;">1</button>
+                    <button style="width:100px;height:100px;">2</button>
+                    <button style="width:100px;height:100px;">3</button>
+                </div>
+                <div class="row">
+                    <button style="width:100px;height:100px;">C</button>
+                    <button style="width:100px;height:100px;">0</button>
+                    <button style="width:100px;height:100px;">.</button>
+                </div>
             </div>
-            <div class="col-xs-2">
-                <div class="row">
-                    <button style="width:50px;height:50px;">7</button>
-                    <button style="width:50px;height:50px;">8</button>
-                    <button style="width:50px;height:50px;">9</button>
+                <div class="col-xs-6">
+                    <table class="table">
+                        <tr>
+                            <td>Kasir: </td>
+                            <td>Sabila Malahati Arnindya</td>
+                            <td>Total:</td>
+                            <td><b>Rp 31.000</b></td>
+                        </tr>
+                        <tr>
+                            <td>Tanggal:</td>
+                            <td>29 Mei 2017</td>
+                            <td>Bayar:</td>
+                            <td>Rp 50.000</td>
+                        </tr>
+                        <tr>
+                            <td>Waktu:</td>
+                            <td>14.20</td>
+                            <td>Kembali:</td>
+                            <td>Rp 19.000</td>
+
+                        </tr>
+                    </table>
                 </div>
-                <div class="row">
-                    <button style="width:50px;height:50px;">4</button>
-                    <button style="width:50px;height:50px;">5</button>
-                    <button style="width:50px;height:50px;">6</button>
+                <div class="col-xs-6">
+                    <h3>Menu Navigasi</h3>
+                    <button style="width:100px;height:100px;"><i class="fa fa-shopping-cart fa-4x"></i>Checkout</button>
+                    <button style="width:100px;height:100px;"><i class="fa fa-plus fa-4x"></i>Tambah</button>
+                    <button style="width:100px;height:100px;"><i class="fa fa-minus fa-4x"></i>Kurang</button>
+                    <button style="width:100px;height:100px;"><i class="fa fa-search fa-4x"></i><br>Cari</button>
                 </div>
-                <div class="row">
-                    <button style="width:50px;height:50px;">1</button>
-                    <button style="width:50px;height:50px;">2</button>
-                    <button style="width:50px;height:50px;">3</button>
-                </div>
-                <div class="row">
-                    <button style="width:103px;height:50px;">0</button>
-                    <button style="width:50px;height:50px;">.</button>
-                </div>
-                
-            </div>
-            <div class="col-xs-2">
-                <button style="width:103px;height:50px;">OK</button>
-                <button style="width:103px;height:50px;">CANCEL</button>
-            </div>
+
         </div>
-        <div class="row">
-            <div class="col-xs-6">
-                <div class="box">
-                    <div class="box-header">
-                        <p>Rincian</p>
-                    </div>
-                    <div class="box-body">
-                        <table class="table table-bordered">
-                            <thead align="center">
-                            <tr>
-                                <td><b>ID Barang</b></td>
-                                <td><b>Nama Barang</b></td>
-                                <td><b>Harga</b></td>
-                            </tr>
-                            </thead>
-                            <tr>
-                                <td>BDSD12321</td>
-                                <td>Oceana</td>
-                                <td>6000</td>
-                            </tr>
-                            <tr>
-                                <td>BDSD12321</td>
-                                <td>Rinso Cair 250ml</td>
-                                <td>10000</td>
-                            </tr>
-                            <tr>
-                                <td>BDSD12231</td>
-                                <td>Baygon Cair 250ml</td>
-                                <td>12000</td>
-                            </tr>
-                            <tr>
-                                <td>BDSD12444</td>
-                                <td>Indomie Rebus Ayam Bawang</td>
-                                <td>3000</td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xs-6">
-                <h3>Menu Navigasi</h3>
-                <button style="width:100px;height:100px;"><i class="fa fa-shopping-cart fa-4x"></i>Checkout</button>
-                <button style="width:100px;height:100px;"><i class="fa fa-plus fa-4x"></i>Tambah</button>
-                <button style="width:100px;height:100px;"><i class="fa fa-minus fa-4x"></i>Kurang</button>
-                <button style="width:100px;height:100px;"><i class="fa fa-search fa-4x"></i><br>Cari</button>
-            </div>
+
         </div>
     </div>
 </div>
